13218787039b6d86d0326ccc852b00d5f8340594
Emanoel <emanoelsaraivas@gmail.com>
2024-05-20 22:22:23 -0300

    post filmes

--- src/index.js
+++ src/index.js
@@ -1,17 +1,24 @@
 import express from 'express'
 import cors from 'cors'
 import bcrypt from 'bcrypt'
+import 'dotenv/config'
+import {v4 as uuid} from 'uuid'
+
 
 const app = express()
 
 app.use(cors())
 app.use(express.json())
 
+const port = process.env.PORT
+
+
 // CRUD de Filmes - Cadastro de usuário/ Cadastro de Filme/ Login/ Listar filmes por genero/ Listar todos os filmes / Atualizar Filmes / Deletar filmes
 
 let users = []
-let idAutomatico = 1
+let filmes = []
 
+// Cadastro de usuário
 app.post('/users', async (req, res) => {
     const { nome, senha, email } = req.body
     //nome = Paulo
@@ -40,17 +47,44 @@ app.post('/users', async (req, res) => {
     const senhaIncriptada = await bcrypt.hash(senha, 10)
 
     const newUser = {
-        id: idAutomatico,
+        id: uuid(),
         nome,
         senha: senhaIncriptada,
         email
     }
-
-    idAutomatico++
 
     users.push(newUser)
 
     return res.status(201).json({ ok: true, message: 'Usuário cadastrado', data: newUser})
 })
 
-app.listen(3333, () => console.log('Server runnig port 3333'))
+app.post('/filmes',(req, res) => {
+    try {
+
+        const tomatoes = Number(req.body.tomatoes)
+
+        const { titulo, genero, duracao, sinopse, dtLanca } = req.body
+
+        if(!titulo || !genero || !duracao || !sinopse || !dtLanca || !tomatoes){
+            return res.status(401).json({ok: false, message: 'Campo não pode estar em branco'})
+        }
+        
+        const newFilme = {
+            id: uuid(),
+            titulo,
+            genero,
+            duracao,
+            sinopse,
+            dtLanca,
+            tomatoes 
+        }
+
+        filmes.push(newFilme)
+
+        return res.status(201).json({ok: true, message: 'Filme cadastrado', data: newFilme})
+    } catch (error) {
+        return res.status(500).json({ok:false, message: 'Internal error'})
+    }
+})
+
+app.listen(port, () => console.log(`Server runnig port ${port}`))
